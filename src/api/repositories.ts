b93d477ceafeb0d46af88907b140bc0b7cbb669d
Felipe Nunes Vaz <felipevnunes@gmail.com>
2019-03-11 09:22:21 -0500

    Log error if get repositories response is not an array

--- src/api/repositories.ts
+++ src/api/repositories.ts
@@ -1,7 +1,12 @@
 import { fetch } from './../fetch';
 import { GITHUB_URL } from './../constants';
 import IRepository from '../interfaces/IRepository';
+import logger, { ELogType } from '../logger';
 
 export const listRepositories = async (organization: string): Promise<Array<IRepository>> => {
-    return fetch(`${GITHUB_URL}orgs/${organization}/repos?per_page=100`).then(res => res.json());
+    return fetch(`${GITHUB_URL}orgs/${organization}/repos?per_page=100`).then(async res => {
+        const repositories = await res.json();
+        if (!Array.isArray(repositories)) logger.log(`Respositories response is not a list: ${JSON.stringify(repositories)}`, ELogType.error);
+        return repositories;
+    });
 }
